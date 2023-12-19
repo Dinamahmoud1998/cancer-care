20ad698dbacd454ffb8518f6dcaff7327463dbd8
Dinamahmoud1998 <154019698+Dinamahmoud1998@users.noreply.github.com>
2023-12-19 20:38:07 +0200

    Add files via upload

---
+++ final project.js
@@ -0,0 +1,17 @@
+function visible(){
+document.getElementById("line").style.display='block'
+
+
+
+}
+
+function non(){
+    document.getElementById("line").style.display='none'
+    
+
+    }
+    
+    function eraseText() {
+        document.getElementById("w3review").value = "";
+        
+    }
